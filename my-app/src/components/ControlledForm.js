2b4dd5d91bc1950a6a1c29d975ca42021deadcab
F-Clement <>
2023-12-28 12:21:45 +0000

    How to submit form in React without page refreshing

--- my-app/src/components/ControlledForm.js
+++ my-app/src/components/ControlledForm.js
@@ -5,7 +5,9 @@ export class ControlledForm extends Component {
         super(props);
 
         this.state = {
-            name: ''
+            name: '',
+            category: 'website',
+            comments: ''
 
         };
     }
@@ -17,22 +19,65 @@ export class ControlledForm extends Component {
 
     };
 
+    handleComments = (event) => {
+        this.setState({
+            comments: event.target.value
+        });
+    };
+
+    handleCategory = (event) => {
+        this.setState({
+            category: event.target.value
+        });
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state);
+    };
+
 
     render() {
         return (
             <div>
-                <form>
-                    <label htmlFor='id.name'>Your Name:</label>
-                    <input
-                        value={this.state.name}
-                        onChange={this.handleNamechange}
-                        id='id.name'
-                        name='name'
-                        type="text"
-                    />
+                <form onSubmit={this.handleSubmit}>
+                    <h2>Please fill out the form below.</h2>
+                    <div>
+                        <label htmlFor='id.name'>Your Name:</label>
+                        <input
+                            value={this.state.name}
+                            onChange={this.handleNamechange}
+                            id='id.name'
+                            name='name'
+                            type="text"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor='id.category'>Query Category:</label>
+                        <select
+                            id='id.category'
+                            name='category'
+                            value={this.state.category}
+                            onChange={this.handleCategory}
+                        >
+                            <option value='website'>Website Issue</option>
+                            <option value='order'>Order Issue</option>
+                            <option value='general'>General Query</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor='id.comments'>Comments</label>
+                        <textarea
+                            id='id.comments'
+                            name='comments'
+                            value={this.state.comments}
+                            onChange={this.handleComments}
+
+                        />
+                    </div>
                     <input type="submit" value="submit" />
                 </form>
-            </div>
+            </div >
         );
     }
 }
