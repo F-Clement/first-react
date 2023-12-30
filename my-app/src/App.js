7e312cab9ef18870b2f675f5b983c715dd09940a
F-Clement <>
2023-12-30 17:33:05 +0000

    Convert stateful class component (ControlledForm) to functional class using Hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -19,6 +19,7 @@ import LifeCyclesCWU from './components/LifeCyclesCWU';
 import ControlledForm from './components/ControlledForm';
 import UncontrolledForm from './components/UncontrolledForm';
 import SearchBar from './components/SearchBar';
+import ControlledFormHooks from './components/ControlledFormHooks';
 
 function App() {
     return (
@@ -40,7 +41,8 @@ function App() {
             {/* <LifeCyclesCWU /> */}
             {/* <ControlledForm /> */}
             {/* <UncontrolledForm /> */}
-            <SearchBar />
+            {/* <SearchBar /> */}
+            <ControlledFormHooks />
         </div>
     );
 }
