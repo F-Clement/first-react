b9a1c191fa04aa5c7bef52964199c591d95cf7e8
F-Clement <>
2023-12-31 00:26:39 +0000

    How to restrict useEffect to run only on specific instances

--- my-app/src/components/UseEffectCounter.js
+++ my-app/src/components/UseEffectCounter.js
@@ -3,8 +3,15 @@ import React, { useState, useEffect } from 'react';
 function UseEffectCounter() {
     const [count, setCount] = useState(0);
     useEffect(() => {
+        console.log("Count 1 effect");
         document.title = count;
-    });
+    }, [count]);
+
+    const [count2, setCount2] = useState(0);
+    useEffect(() => {
+        console.log("Count 2 effect");
+        document.title = count2;
+    }, [count2]);
 
     return (
         <div>
@@ -12,6 +19,12 @@ function UseEffectCounter() {
                 () => setCount(count => count + 1)
             }>
                 Increment Count {count}
+            </button>
+
+            <button onClick={
+                () => setCount2(count2 => count2 + 1)
+            }>
+                Increment Count2 {count}
             </button>
         </div>
     );
