94389f9edc1d6ecd2aa23f15f2cfea6f06620d3e
F-Clement <>
2023-12-31 14:11:28 +0000

    Restrict effect to run just once after the component mounts

--- my-app/src/components/UseEffectCounter.js
+++ my-app/src/components/UseEffectCounter.js
@@ -2,30 +2,33 @@ import React, { useState, useEffect } from 'react';
 
 function UseEffectCounter() {
     const [count, setCount] = useState(0);
+    const [time, setTime] = useState(0);
+
     useEffect(() => {
         console.log("Count 1 effect");
         document.title = count;
     }, [count]);
 
-    const [count2, setCount2] = useState(0);
+
     useEffect(() => {
-        console.log("Count 2 effect");
-        document.title = count2;
-    }, [count2]);
+        console.log('Creating timer');
+        const interval = setInterval(() => {
+            //while learning we had + 1 but it doubles the count for me
+            //So when I used 0.5 it worked.
+            setTime(time => time + 0.5);
+        }, 1000);
+    }, []);
+
 
     return (
         <div>
             <button onClick={
                 () => setCount(count => count + 1)
             }>
-                Increment Count {count}
+                Increment Count ({count})
             </button>
 
-            <button onClick={
-                () => setCount2(count2 => count2 + 1)
-            }>
-                Increment Count2 {count}
-            </button>
+            <h2>Time is {time}</h2>
         </div>
     );
 }
